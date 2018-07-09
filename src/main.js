ecc1e24b25475829abb767c7b39f304d650c1e19
James Taber <JATaber@fullsail.edu>
2018-07-09 14:16:44 -0500

    added VueAwsome to use Font Awesome icons

--- src/main.js
+++ src/main.js
@@ -6,8 +6,10 @@ import router from './router'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue'
 import BootstrapVue from 'bootstrap-vue'
+import Icon from 'vue-awesome/components/Icon'
 
 Vue.use(BootstrapVue)
+Vue.component('icon', Icon)
 
 Vue.config.productionTip = false
 
